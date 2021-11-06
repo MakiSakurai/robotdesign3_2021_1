4d610ef83424faf528876db3b6d888b7f2cdf46b
KoheiYanagisawa <yanahei0514@gmail.com>
2021-11-06 21:14:22 +0900

    add printCoordinate

--- hand_tracking/test2.py
+++ hand_tracking/test2.py
@@ -23,12 +23,32 @@ with mp_hands.Hands(
     # pass by reference.
     image.flags.writeable = False
     results = hands.process(image)
-
+    image_height, image_width, _ = image.shape
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
+        for index, landmark in enumerate(hand_landmarks.landmark):
+            cx,cy = landmark.x * image_width, landmark.y * image_height
+            if index == 0: #手首
+                print("index0 = ",end='')
+                print(cx,cy)
+            if index == 3: #親指第一関節
+                print("index3 = ",end='')
+                print(cx,cy)
+            if index == 7: #人差し指第一関節
+                print("index7 = ",end='')
+                print(cx,cy)
+            if index == 11: #中指第一関節
+                print("index11 = ",end='')
+                print(cx,cy)
+            if index == 15: #薬指第一関節
+                print("index15 = ",end='')
+                print(cx,cy)
+            if index == 19: #小指第一関節
+                print("index19 = ",end='')
+                print(cx,cy)
         mp_drawing.draw_landmarks(
             image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
     cv2.imshow('MediaPipe Hands', image)
