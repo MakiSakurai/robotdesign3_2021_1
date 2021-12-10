e72901ee422591b6ee63053fade4954019fa91b0
MakiSakurai <s20c1047sb@s.chibakoudai.jp>
2021-12-10 19:58:07 +0900

    subを追加 & first commit

--- scripts/sub_point.py
+++ scripts/sub_point.py
@@ -8,32 +8,31 @@ import math
 import tf2_ros
 from geometry_msgs.msg import Point
 from tf.transformations import quaternion_from_euler
+
 class tekitou():
     def __init__(self):
         rospy.init_node('hoge')
         rospy.Subscriber('/hand_topic', Point, self.callback, queue_size=1)
 
     def callback(self, data):
-        self.t_x = data.x
-        self.t_y = data.y
-        self.t_z = data.z
+        t_x = data.x
+        t_y = data.y
+        t_z = data.z
+        print(t_x)
 
     def loop(self):
-        rate = rospy.Rate(10.0)
-        rate.sleep()
-        trans = tfBuffer.lookup_transform("base_link", "camera_base", rospy.Time(0))
-        plus_x = trans.transform.translation.x + self.t_x
-        plus_y = trans.transform.translation.y + self.t_y
-        plus_z = trans.transform.translation.z + self.t_z
-        print(plus_x, plus_y, plus_z)
-        rate.sleep()
+        print("makki")
+
 
 def main():
+
     makimaki = tekitou()
+    rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
         makimaki.loop()
+        rate.sleep()
+
+
             
 if __name__ == '__main__':   
-    tfBuffer = tf2_ros.Buffer()
-    listener = tf2_ros.TransformListener(tfBuffer)
     main()
