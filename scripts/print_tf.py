90d9f00ff33df7beb82651770b75f45602ba7e38
MakiSakurai <s20c1047sb@s.chibakoudai.jp>
2021-12-11 14:00:08 +0900

    バッファを作成する箇所を変更

--- scripts/print_tf.py
+++ scripts/print_tf.py
@@ -10,6 +10,7 @@ from geometry_msgs.msg import Point
 from tf.transformations import quaternion_from_euler
 class tekitou():
     def __init__(self):
+
         rospy.init_node('hoge')
         rospy.Subscriber('/hand_topic', Point, self.callback, queue_size=1)
 
@@ -20,20 +21,26 @@ class tekitou():
 
     def loop(self):
         rate = rospy.Rate(10.0)
+        tfBuffer = tf2_ros.Buffer()
+        listener = tf2_ros.TransformListener(tfBuffer)
+        try:
+            trans = tfBuffer.lookup_transform("base_link", "camera_base", rospy.Time(0))
+        except Exception as e:
+            print("muri")
         rate.sleep()
-        trans = tfBuffer.lookup_transform("base_link", "camera_base", rospy.Time(0))
         plus_x = trans.transform.translation.x + self.t_x
         plus_y = trans.transform.translation.y + self.t_y
         plus_z = trans.transform.translation.z + self.t_z
         print(plus_x, plus_y, plus_z)
-        rate.sleep()
+
 
 def main():
     makimaki = tekitou()
+    
+
     while not rospy.is_shutdown():
         makimaki.loop()
+ 
             
 if __name__ == '__main__':   
-    tfBuffer = tf2_ros.Buffer()
-    listener = tf2_ros.TransformListener(tfBuffer)
     main()
