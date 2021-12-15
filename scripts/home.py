ed871002a74a4f48b540a8de6d0af62dd062006e
MakiSakurai <nikumann8@gmail.com>
2021-12-15 15:02:07 +0900

    グリッパー開閉機能を追加

--- scripts/home.py
+++ scripts/home.py
@@ -4,16 +4,53 @@
 import rospy
 import sys
 import moveit_commander
+import actionlib
 import geometry_msgs.msg
 import rosnode
 from tf import transformations
 from tf.transformations import quaternion_from_euler
 import math
+from std_msgs.msg import Float64
+from control_msgs.msg import (
+    GripperCommandAction,
+    GripperCommandGoal
+)
+class Home(object):
 
+    def __init__(self):
+        rospy.init_node("pose_groupstate_example")
+        self._client = actionlib.SimpleActionClient("/crane_x7/gripper_controller/gripper_cmd",GripperCommandAction)
+        self._goal = GripperCommandGoal()
+        self.robot = moveit_commander.RobotCommander()
+        # Wait 10 Seconds for the gripper action server to start or exit
+        self._client.wait_for_server(rospy.Duration(10.0))
+        if not self._client.wait_for_server(rospy.Duration(10.0)):
+            rospy.logerr("Exiting - Gripper Action Server Not Found.")
+            rospy.signal_shutdown("Action Server not found.")
+            sys.exit(1)
+        self.clear()
+
+    def command(self, position, effort):
+        self._goal.command.position = position
+        self._goal.command.max_effort = effort
+        self._client.send_goal(self._goal,feedback_cb=self.feedback)
+
+    def feedback(self,msg):
+        print("feedback callback")
+        print(msg)
+
+    def stop(self):
+        self._client.cancel_goal()
+
+    def wait(self, timeout=0.1 ):
+        self._client.wait_for_result(timeout=rospy.Duration(timeout))
+        return self._client.get_result()
+
+    def clear(self):
+        self._goal = GripperCommandGoal()
 
 def main():
-    rospy.init_node("pose_groupstate_example")
-    robot = moveit_commander.RobotCommander()
+    gc = Home()
     arm = moveit_commander.MoveGroupCommander("arm")
     arm.set_max_velocity_scaling_factor(0.1)
     arm.set_max_acceleration_scaling_factor(1.0)
@@ -21,47 +58,42 @@ def main():
     arm.set_max_velocity_scaling_factor(0.5)
     arm.set_max_acceleration_scaling_factor(1.0)
     target_joint_values = arm.get_current_joint_values()
-
     while len([s for s in rosnode.get_node_names() if 'rviz' in s]) == 0:
         rospy.sleep(1.0)
     rospy.sleep(1.0)
-
     key = raw_input()
-    print(key)
+
+    if key == "o": 
+        print("Open Gripper.")
+        gripper = 45.0
+        gc.command(math.radians(gripper),1.0)
+        result = gc.wait(2.0)
+        print(result)
+        rospy.sleep(1.0)
+        print("")
+
+    if key == "c": 
+        print("Close Gripper.")
+        gripper = 0.0 # 0.0
+        gc.command(math.radians(gripper),1.0)
+        result = gc.wait(2.0)
+        print(result)
+        rospy.sleep(1.0)
+    
 
     if key == "h":    
-        #駆動速度設定（通常の２分の１ほどに設定）
         arm.set_max_velocity_scaling_factor(0.25)
         arm.set_max_acceleration_scaling_factor(0.5)
-        #一度verticalの姿勢にする
         print("vertical")
         arm.set_named_target("vertical")
         arm.go()
-
-        """""
-        target_pose = geometry_msgs.msg.Pose()
-        target_pose.position.x = 0.
-        target_pose.position.y = 0.1
-        target_pose.position.z = 0.6
-        q = quaternion_from_euler( 0.0, 0.0, 0.0 )
-        target_pose.orientation.x = q[0]
-        target_pose.orientation.y = q[1]
-        target_pose.orientation.z = q[2]
-        target_pose.orientation.w = q[3]
-        arm.set_pose_target( target_pose )
-        arm.go()
-        #target_joint_values[5] = math.radians(-30)
-        #arm.set_joint_value_target(target_joint_values)
-        #arm.go()
-        print("gre")
-        """
         target_joint_values = arm.get_current_joint_values()
         target_joint_values[1] = math.radians(-10)
         target_joint_values[2] = math.radians(0)
         target_joint_values[3] = math.radians(-80)
         target_joint_values[4] = math.radians(0)
         target_joint_values[5] = math.radians(-90)
-        target_joint_values[6] = math.radians(0)
+        target_joint_values[6] = math.radians(90)
         arm.set_joint_value_target(target_joint_values)
         arm.go()
         print("current_joint_values (radians):")
@@ -69,37 +101,7 @@ def main():
         arm_goal_pose = arm.get_current_pose().pose
         print("Arm goal pose:")
         print(arm_goal_pose)
-    """""
-    if key == "t":    
-        target_pose = geometry_msgs.msg.Pose()
-        target_pose.position.x = 0.0
-        target_pose.position.y = 0.0
-        target_pose.position.z = 0.4
-        q = quaternion_from_euler( 0.0, 0.0, 0.0 )
-        target_pose.orientation.x = q[0]
-        target_pose.orientation.y = q[1]
-        target_pose.orientation.z = q[2]
-        target_pose.orientation.w = q[3]
-        arm.set_pose_target( target_pose )
-        arm.go()
 
-    if key == "z":    
-        target_pose = geometry_msgs.msg.Pose()
-        target_pose.position.x = 0.0
-        target_pose.position.y = 2.0
-        target_pose.position.z = 0.0
-        q = quaternion_from_euler( 0.0, 0.0, 0.0 )
-        target_pose.orientation.x = q[0]
-        target_pose.orientation.y = q[1]
-        target_pose.orientation.z = q[2]
-        target_pose.orientation.w = q[3]
-        arm.set_pose_target( target_pose )
-        arm.go()
-    arm_goal_pose = arm.get_current_pose().pose
-    print("Arm goal pose:")
-    print(arm_goal_pose)
-    print("done")
-    """
     if key == "f":
         print("vertical")
         arm.set_named_target("vertical")
